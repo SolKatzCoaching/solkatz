8d0275b051228e9d0ee352fb6aa21d17e1c1e624
SolKatzCoaching <coaching@solkatz.com>
2026-05-25 16:45:55 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Arial"
+      }}
+    >
+      <div>
+        <h1>SolKatz</h1>
+        <p>Your Guide on the Side</p>
+      </div>
+    </div>
+  );
+}
